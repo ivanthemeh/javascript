efce370794230f3f391826e34000e83782a264c0
Thomas J Bradley <hey@thomasjbradley.ca>
2014-01-16 14:11:40 -0500

    A few fixes and adding of missing code.

--- carousel/js/fade.js
+++ carousel/js/fade.js
@@ -1,21 +1,32 @@
-var $imgs = $('.slides img');
+var $items = $('.carousel-items img');
 
-var switchSlide = function (current, next) {
-  $imgs.eq(next).attr('data-state', 'incoming').fadeIn(250, function () {
-    $imgs.eq(current).attr('data-state', '').hide();
-    $imgs.eq(next).attr('data-state', 'current');
-  });
+var switchSlide = function (outgoing, incoming) {
+	$items.eq(incoming).attr('data-state', 'incoming').fadeIn(250, function () {
+		$items.eq(outgoing).attr('data-state', '').hide();
+		$items.eq(incoming).attr('data-state', 'current');
+	});
 };
 
-$imgs.filter(':not([data-state="current"])').hide();
+$items.filter(':not([data-state="current"])').hide();
 
 $('.next').on('click', function () {
-  var current = $imgs.filter('[data-state="current"]').index();
-  var next = current + 1;
+	var current = $items.filter('[data-state="current"]').index();
+	var next = current + 1;
 
-  if (next > $imgs.length - 1) {
-    next = 0;
-  }
+	if (next >= $items.length) {
+		next = 0;
+	}
 
-  switchSlide(current, next);
+	switchSlide(current, next);
+});
+
+$('.prev').on('click', function () {
+	var current = $items.filter('[data-state="current"]').index();
+	var prev = current - 1;
+
+	if (prev < 0) {
+		prev = $items.length - 1;
+	}
+
+	switchSlide(current, prev);
 });
--- tabs/js/tabs.js
+++ tabs/js/tabs.js
@@ -4,12 +4,12 @@ var $tabPanels = $('.tab-panel');
 $tabPanels.filter(':not([data-state="current"])').hide();
 
 $tabs.on('click', function () {
-  // $(this) gives us access to the thing that was clicked on
-  var id = $(this).attr('href');
+	// $(this) gives us access to the thing that was clicked on
+	var id = $(this).attr('href');
 
-  $tabPanels.filter('[data-state="current"]').hide();
-  $(id).show().attr('data-state', 'current');
+	$tabPanels.filter('[data-state="current"]').hide();
+	$(id).show().attr('data-state', 'current');
 
-  $tabs.attr('data-state', '');
-  $(this).attr('data-state', 'current');
+	$tabs.attr('data-state', '');
+	$(this).attr('data-state', 'current');
 });
