c98466d43f7160cacc7217ec22fca70de9b6e872
Thomas J Bradley <hey@thomasjbradley.ca>
2015-02-24 18:23:29 -0500

    Add event delegation tutorial code.

--- dom/js/forms.js
+++ dom/js/forms.js
@@ -4,13 +4,17 @@ var $age = $('.age');
 var $dinos = $('.dinos');
 
 $form.on('submit', function (e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	var $li = $('<li>');
-	var $nameH2 = $('<h2>').html($name.val());
-	var $ageP = $('<p>').html('Age: ' + $age.val() + ' million years old');
-	var $diet = $('[name="diet"]:checked')
+  var $li = $('<li>');
+  var $nameH2 = $('<h2>').html($name.val());
+  var $ageP = $('<p>').html('Age: ' + $age.val() + ' million years old');
+  var $diet = $('[name="diet"]:checked')
 
-	$li.append($nameH2).append($ageP).addClass($diet.val());
-	$dinos.append($li);
+  $li.append($nameH2).append($ageP).addClass($diet.val());
+  $dinos.append($li);
+});
+
+$dinos.on('click', 'li', function () {
+  $(this).toggleClass('js-highlight');
 });
